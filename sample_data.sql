7330ec83c717e064ba4a0b172c0cbcc89781248e
petrovskidavid <81729777+petrovskidavid@users.noreply.github.com>
2022-04-15 15:01:21 -0500

    Added db and sample data sql files

---
+++ sample_data.sql
@@ -0,0 +1,189 @@
+/**
+ * sample_data.sql
+ * 
+ * David Petrovski, Isabelle Coletti, Amanda Zedwick
+ * CSCI 466 - 1
+ */
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'Dog Food',
+	'25.50',
+	'500' 
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'Food Bowl',
+	'7.00',
+	'250'
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'A Very Special Shampoo',
+	'21.55',
+	'250' 
+);
+
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(	
+	'Collar (Small)',
+	'8',
+	'300' 
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'Collar (Large)',
+	'10',
+	'300' 
+);
+
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(	
+	'Leash',
+	'12.99',
+	'400' 
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(	
+	'Food Mat',
+	'28.99',
+	'429' 
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'Pet Bed (Small)',
+	'99.99',
+	'300'
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(	
+	'Pet Bed (Large)',
+	'150.99',
+	'350' 
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'Brush',
+	'9.99',
+	'400'
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'Pet Cologne',
+	'20.99',
+	'500' 
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'Pet Perfume',
+	'25.99',
+	'400'
+);
+
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'Logo Pet Hoodie (Small)',
+	'30.50',
+	'100'
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(	
+	'Logo Pet Hoodie (Large)',
+	'35.50',
+	'150' 
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(	
+	'Hot Dog Toy (Squeaky)',
+	'69.99',
+	'600'
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(	
+	'Tarantula',
+	'150.00',
+	'10' 
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'Chinchilla',
+	'200.00',
+	'12'
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'Baby Fox',
+	'499.99',
+	'13' 
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(
+	'Chameleon',
+	'299.99',
+	'5'
+);
+
+INSERT INTO Products
+(Name, Price, Quantity)
+VALUES
+(	
+	'Bearded Dragon (Bob-Made)',
+	'999.99',
+	'6' 
+);
---
+++ store_db.sql
@@ -0,0 +1,76 @@
+/**
+ * store_db.sql
+ * 
+ * David Petrovski, Isabelle Coletti, Amanda Zedwick
+ * CSCI 466 - 1
+ */
+DROP TABLE IF EXISTS Carts, ProcessOrders, Customers, Products, Orders, Employees;
+
+
+-- Employees table
+CREATE TABLE Employees(
+    EmpID    INTEGER  AUTO_INCREMENT, -- Employees ID for each employee
+    Name     CHAR(64) NOT NULL,       -- Employees Name
+    Password CHAR(8)  NOT NULL,       -- Password for Employee login (8 char long)
+
+    PRIMARY KEY(EmpID)                -- Sets the primary key (EmpID)
+);
+
+
+-- Orders table
+CREATE TABLE Orders(
+    OrderID     INTEGER AUTO_INCREMENT, -- Order number for each order
+    Status      INTEGER DEFAULT 1,      -- Status of order (1 = in cart (default), 2 = received, 3 = processed)
+    TrackingNum INTEGER,                -- Tracking number of order, initially null untill shipped
+    Notes       CHAR(64),               -- Notes for order, intially null
+    Address     CHAR(64),               -- Address where the order should be shipped, null untill order is placed
+
+    PRIMARY KEY(OrderID)                -- Sets the primary key (OrderID)
+);
+
+
+-- Products table
+CREATE TABLE Products(
+    ProductID INTEGER  AUTO_INCREMENT, -- Product ID for each product
+    Name      CHAR(64) NOT NULL,       -- Name of the product
+    Price     DOUBLE   NOT NULL,       -- Price of the product
+    Quantity  INTEGER  NOT NULL,       -- Quantity of the product in stock
+
+    PRIMARY KEY(ProductID)             -- Sets the primary key (ProductID)
+);
+
+
+-- Customers table
+CREATE TABLE Customers(
+    Username CHAR(64) NOT NULL, -- Customers username
+    Password CHAR(8)  NOT NULL, -- Password for customers login (8 char long)
+    Name     CHAR(64) NOT NULL, -- Customers name
+    Email    CHAR(64) NOT NULL, -- Customers email
+
+    PRIMARY KEY(Username)       -- Sets the primary key (Username)
+);
+
+
+-- Order Process table
+CREATE TABLE ProcessOrders(
+    EmpID   INTEGER NOT NULL,                       -- Employees ID for each employee
+    OrderID INTEGER NOT NULL,                       -- Order number for each order
+
+    PRIMARY KEY(EmpID, OrderID),                    -- Sets the primary key (EmpID, OrderID)
+    FOREIGN KEY(EmpID) REFERENCES Employees(EmpID), -- Sets the foreign key from Employees table
+    FOREIGN KEY(OrderID) REFERENCES Orders(OrderID) -- Sets the foreign key from Orders table
+);
+
+
+-- Shopping Carts table
+CREATE TABLE Carts(
+    OrderID INTEGER NOT NULL,                              -- Order number for each order
+    ProductID INTEGER NOT NULL,                            -- Product ID for each product
+    Username CHAR(64) NOT NULL,                            -- Customers username
+    Amount INTEGER NOT NULL,                               -- Amount of the product to be placed in the cart for the specific customer
+
+    PRIMARY KEY(OrderID, ProductID),                       -- Sets the primary key (OrderID, ProductID)
+    FOREIGN KEY(ProductID) REFERENCES Products(ProductID), -- Sets the foreign key from Products table
+    FOREIGN KEY(OrderID) REFERENCES Orders(OrderID),       -- Sets the foreign key from Orders table
+    FOREIGN KEY(Username) REFERENCES Customers(Username)   -- Sets the foreign key from Customers table
+);
