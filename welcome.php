7169b74a5a8d7202b054cc25635ced2a2bd8b4ed
Isabelle Coletti <z1900409@turing.cs.niu.edu>
2022-04-18 12:45:55 -0500

    Added comments

--- welcome.php
+++ welcome.php
@@ -11,7 +11,7 @@
     include("secrets.php"); // Gives the file with the DB login credentials
 
     try
-    { // Tries to log into database.
+    { // Tries to log into database
 
         $dsn = "mysql:host=courses;dbname=".$username;
         $pdo = new PDO($dsn, $username, $password);
@@ -19,9 +19,9 @@
 
     }
     catch(PDOexception $e)
-    {
+    { // Otherwise catch error message
 
-        // Prints error message.
+        // Prints error message
         echo "Connection to database failed: ".$e->getMessage();
     }
 ?>
