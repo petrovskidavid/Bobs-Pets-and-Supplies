7d7e39e26324f6eb5c4c09675bcf98e071dfe167
David Petrovski <z1894079@turing.cs.niu.edu>
2022-04-19 10:31:20 -0500

    Added doc boxes and made employee_login page not have login button

---
+++ src/employee_login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Employee Login</title>  
+    <link rel="stylesheet" type="text/css" href="../assets/css/body.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/header.css" />
+</head>
+
+<body>
+    <?php
+        /**
+         * employee_login.php
+         * 
+         * David Petrovski, Isabelle Coletti, Amanda Zedwick
+         * 
+         * CSCI 466 - 1
+         */
+
+        include("../src/header.php"); // Gives the file with the header for the top of the page
+        //include("../src/secrets.php"); // Gives the file with the DB login credentials
+
+        echo "This is the employee login site";
+
+
+    ?>
+</body>
+</html>
--- src/header.php
+++ src/header.php
@@ -1,5 +1,12 @@
 <?php
-    
+    /**
+     * header.php
+     * 
+     * David Petrovski, Isabelle Coletti, Amanda Zedwick
+     * 
+     * CSCI 466 - 1
+     */
+
     // Creates the header 
     echo "<header>";
 
@@ -9,18 +16,17 @@
         echo "<img src=\"./assets/img/logo.png\" alt=\"Bob's Pets and Supplies Logo\" class=\"logo\" />";
         // Updates href link to accomodate index.phps location in the repo
         echo "<a href=\"./src/employee_login.php\">"; 
+        
+        // Puts the text and creates a div so we can manipualte how it looks
+        echo "<div class=\"e_login\">Employee Login</div>";
+        echo "</a>";
 
     } else {                                               // Otherwise we are in the src directory
         
         // Updates src link to accomodate the files location in the repo
         echo "<img src=\"../assets/img/logo.png\" alt=\"Bob's Pets and Supplies Logo\" class=\"logo\" />";
-        // Updates href link to accomodate the files location in the repo
-        echo "<a href=\"../src/employee_login.php\">";
+        
     }
-
-    // Puts the text and creates a div so we can manipualte how it looks
-    echo "<div class=\"e_login\">Employee Login</div>";
-    echo "</a>";
 
     // Prints the name of the store
     echo "<h2>Bob's Pets and Supplies</h2>";
