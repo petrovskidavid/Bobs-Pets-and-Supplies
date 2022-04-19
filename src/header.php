c8e65b2404994704d02bb9622ca8a2bbc82de52f
David Petrovski <z1894079@turing.cs.niu.edu>
2022-04-19 10:22:24 -0500

    Fixed to accomodate location of file with header

--- src/header.php
+++ src/header.php
@@ -1,13 +1,22 @@
 <?php
-
-    // Loads logo
-    echo "<img src=\"../assets/img/logo.png\" alt=\"Bob's Pets and Supplies Logo\" class=\"logo\" />";
-
+    
     // Creates the header 
     echo "<header>";
 
-    // Creates the employee login hyperlink area to change the page
-    echo "<a href=\"employee_login.php\">";
+    if (substr($_SERVER['PHP_SELF'], -9)  == "index.php"){ // Checks if we are in the index.php
+        
+        // Updates src link to accomodate index.phps location in the repo
+        echo "<img src=\"./assets/img/logo.png\" alt=\"Bob's Pets and Supplies Logo\" class=\"logo\" />";
+        // Updates href link to accomodate index.phps location in the repo
+        echo "<a href=\"./src/employee_login.php\">"; 
+
+    } else {                                               // Otherwise we are in the src directory
+        
+        // Updates src link to accomodate the files location in the repo
+        echo "<img src=\"../assets/img/logo.png\" alt=\"Bob's Pets and Supplies Logo\" class=\"logo\" />";
+        // Updates href link to accomodate the files location in the repo
+        echo "<a href=\"../src/employee_login.php\">";
+    }
 
     // Puts the text and creates a div so we can manipualte how it looks
     echo "<div class=\"e_login\">Employee Login</div>";
