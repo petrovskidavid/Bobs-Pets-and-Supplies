7f044634931c0c2aa830b9e71e6977a728cdcc94
David Petrovski <z1894079@turing.cs.niu.edu>
2022-04-19 00:54:53 -0500

    Created header with name of store and logo of the store

--- welcome.php
+++ welcome.php
@@ -1,4 +1,42 @@
-<!DOCTYPE html><html><head><title>Welcome!</title></head><body>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome!</title>
+    <style>   
+        header 
+        {
+            text-align: left;           /* Alligns text in the header to the left */
+            background: #3bbfa0;        /* Background color of the header element */
+            padding: 12px;              /* Creates a space around the header to make it distinct */
+            font-family: Comic Sans MS; /* Font for the text in the header */
+
+            /* Creates a solid border with a width of 3 pixels and beige color */
+            border: 3px;                
+            border-style: solid;        
+            border-color: beige; 
+        }
+
+        .logo
+        {
+            float: left;        /* Makes the logo go to the left of the page */
+            margin-right: 20px; /* Gives space to the right for the text */
+            width: 100px;       /* Width of the logo */
+            height: 100px;      /* Height of the logo */
+        }
+
+        body 
+        {
+            background: #f7c27c; /* Background color of the whole page */
+            margin: 0;           /* Makes the header go to the top of the screen */
+        }
+    </style>
+</head>
+
+<body>
+<img src="assets/img/logo.png" alt="Bob's Pets and Supplies Logo" class="logo">
+<header>
+    <h2>Bob's Pets and Supplies</h2>
+</header>
 <?php
     /**
      * welcome.php
