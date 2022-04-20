af525119182ac4092426bebc5dc690fdec3b5623
David Petrovski <z1894079@turing.cs.niu.edu>
2022-04-20 17:33:17 -0500

    Added employee home page file

---
+++ src/employee_home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Employee Home</title>  
+    <link rel="stylesheet" type="text/css" href="../assets/css/body.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/header.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/login.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/button.css" />
+</head>
+
+<body>
+    <?php
+        /**
+         * @file employee_home.php
+         * 
+         * @brief This is the employee home page.
+         *        This is where they can access the orders they need to process.
+         * 
+         * @author David Petrovski
+         * @author Isabelle Coletti
+         * @author Amanda Zedwick
+         * 
+         * CSCI 466 - 1
+         */
+
+         
+        include("header.php"); // Creates the header of the page
+        include("secrets.php"); // Logs into the db
+        include("functions.php"); // Gives the file with the login window creation function
+
+
+        echo "This is the employee home page.";
+    ?>
+</body></html>
