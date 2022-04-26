ab908e993d28f1b82fe6aef32cb1c83129ff4293
David Petrovski <z1894079@turing.cs.niu.edu>
2022-04-26 09:52:13 -0500

    Added description for products

--- sql/sample_data.sql
+++ sql/sample_data.sql
@@ -12,10 +12,11 @@
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'Dog Food',
+	' ',
 	'25.50',
 	'500',
 	'https://images2.imgbox.com/e7/74/OhlW7DXn_o.png'
@@ -23,10 +24,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'Food Bowl',
+	' ',
 	'7.00',
 	'250',
 	'https://images2.imgbox.com/02/fb/bqz3I1Jp_o.png'
@@ -34,10 +36,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'A Very Special Shampoo',
+	' ',
 	'21.55',
 	'250',
 	'https://images2.imgbox.com/f1/d9/CyLrmKd2_o.png' 
@@ -45,10 +48,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (	
 	'Collar (Small)',
+	' ',
 	'8',
 	'300',
 	'https://images2.imgbox.com/6f/e9/2nuYpCIw_o.png' 
@@ -56,10 +60,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'Collar (Large)',
+	' ',
 	'10',
 	'300',
 	'https://images2.imgbox.com/b3/07/2IjAqifZ_o.png' 
@@ -67,10 +72,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (	
 	'Leash',
+	' ',
 	'12.99',
 	'400',
 	'https://images2.imgbox.com/0f/e3/T4jkbFKB_o.png' 
@@ -78,10 +84,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (	
 	'Food Mat',
+	' ',
 	'28.99',
 	'429',
 	'https://images2.imgbox.com/52/f2/3TxdsyIy_o.png' 
@@ -89,10 +96,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'Pet Bed (Small)',
+	' ',
 	'99.99',
 	'300',
 	'https://images2.imgbox.com/6d/e4/iI6K6W96_o.png'
@@ -100,10 +108,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (	
 	'Pet Bed (Large)',
+	' ',
 	'150.99',
 	'350',
 	'https://images2.imgbox.com/9b/e8/m8Ib3y5E_o.png' 
@@ -111,10 +120,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'Brush',
+	' ',
 	'9.99',
 	'400',
 	'https://images2.imgbox.com/98/af/LzKtEsvQ_o.png'
@@ -122,10 +132,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'Pet Cologne',
+	' ',
 	'20.99',
 	'500',
 	'https://images2.imgbox.com/c9/2d/KgMapJ2g_o.png' 
@@ -133,10 +144,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'Pet Perfume',
+	' ',
 	'25.99',
 	'400',
 	'https://images2.imgbox.com/1d/89/oRhBNPYl_o.png'
@@ -144,10 +156,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'Logo Pet Hoodie (Small)',
+	' ',
 	'30.50',
 	'100',
 	'https://images2.imgbox.com/9a/d4/FpheE4Yi_o.png'
@@ -155,10 +168,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (	
 	'Logo Pet Hoodie (Large)',
+	' ',
 	'35.50',
 	'150',
 	'https://images2.imgbox.com/bb/73/qgLXC5PE_o.png' 
@@ -166,10 +180,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (	
 	'Hot Dog Toy (Squeaky)',
+	' ',
 	'69.99',
 	'600',
 	'https://images2.imgbox.com/40/05/HArcYloD_o.png'
@@ -177,10 +192,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (	
 	'Tarantula',
+	' ',
 	'150.00',
 	'10',
 	'https://images2.imgbox.com/4a/27/7SdP91xU_o.png' 
@@ -188,10 +204,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'Chinchilla',
+	' ',
 	'200.00',
 	'12',
 	'https://images2.imgbox.com/75/94/xdIsSm6a_o.png'
@@ -199,10 +216,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'Baby Fox',
+	' ',
 	'499.99',
 	'13',
 	'https://images2.imgbox.com/84/1b/TpaehIoJ_o.png' 
@@ -210,10 +228,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (
 	'Chameleon',
+	' ',
 	'299.99',
 	'5',
 	'https://images2.imgbox.com/87/94/2hgIpDhh_o.png'
@@ -221,10 +240,11 @@ VALUES
 
 
 INSERT INTO Products
-(Name, Price, Quantity, ImgLink)
+(Name, Description, Price, Quantity, ImgLink)
 VALUES
 (	
 	'Bearded Dragon (Bob-Made)',
+	' ',
 	'999.99',
 	'6',
 	'https://images2.imgbox.com/eb/03/cR7fFMle_o.png' 
