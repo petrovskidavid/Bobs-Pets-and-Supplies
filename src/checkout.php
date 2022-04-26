438a6c555b58eca1e0caaca9c05e976667da81f6
David Petrovski <z1894079@turing.cs.niu.edu>
2022-04-26 12:32:04 -0500

    Added comments

--- src/checkout.php
+++ src/checkout.php
@@ -34,6 +34,10 @@
 
         // Creates a return button to the cart page.
 		create_return_btn("./cart.php", 1);
-    
+
+
+        /* Make sure to delete the order from the Cart after checkout is processed, assign an employee to the order, update the status of the order and also update
+           the products quantity after the order is sucesfully checked out.
+        */
     ?>
 </body></html>
