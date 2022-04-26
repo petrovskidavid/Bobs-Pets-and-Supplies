58c82b6b72d11315b32c520cc0e6803928e0c4b5
Isabelle Coletti <z1900409@hopper.cs.niu.edu>
2022-04-26 17:34:41 -0500

    Added product descriptions

--- sql/sample_data.sql
+++ sql/sample_data.sql
@@ -16,7 +16,7 @@ INSERT INTO Products
 VALUES
 (
 	'Dog Food',
-	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec blandit dui at consectetur pulvinar. Aliquam vehicula odio eu tortor blandit, vel bibendum nisl pretium. Aenean ut volutpat orci. Proin vitae nisl lorem. Vivamus scelerisque',
+	'Give your best friend all the energy and nutrition they need to live life to the fullest. This formula includes all the necessary vitamins and minerals for your pooch, with a delicious beef flavor.',
 	'25.50',
 	'500',
 	'https://images2.imgbox.com/e7/74/OhlW7DXn_o.png'
@@ -28,7 +28,7 @@ INSERT INTO Products
 VALUES
 (
 	'Food Bowl',
-	' ',
+	'The highest quality stainless steel pet food bowl on the market. Includes an anti-microbial surface to keep the germs away from your precious pet.',
 	'7.00',
 	'250',
 	'https://images2.imgbox.com/02/fb/bqz3I1Jp_o.png'
@@ -40,7 +40,7 @@ INSERT INTO Products
 VALUES
 (
 	'A Very Special Shampoo',
-	' ',
+	'The most special shampoo formula ever created, with secret ingredient aloe to nourish your pet''s skin. The packaging is also 100% recyclable, so it is healthy for pets, people, and the planet.',
 	'21.55',
 	'250',
 	'https://images2.imgbox.com/f1/d9/CyLrmKd2_o.png' 
@@ -52,7 +52,7 @@ INSERT INTO Products
 VALUES
 (	
 	'Collar (Small)',
-	' ',
+	'A chew-proof, olive green collar made from nylon with an easy side release buckle. The diameter measures 4 inches across. Recommended for small dog breeds or large cats.',
 	'8',
 	'300',
 	'https://images2.imgbox.com/6f/e9/2nuYpCIw_o.png' 
@@ -64,7 +64,7 @@ INSERT INTO Products
 VALUES
 (
 	'Collar (Large)',
-	' ',
+	'A heavy-duty black and tan leather collar. Includes a traditional buckle for security and decorative gold plating. Diameter measures 7 inches across. Ideal for large dog breeds.',
 	'10',
 	'300',
 	'https://images2.imgbox.com/b3/07/2IjAqifZ_o.png' 
@@ -76,7 +76,7 @@ INSERT INTO Products
 VALUES
 (	
 	'Leash',
-	' ',
+	'Keep your pet with you at all times with this 7-foot-long sturdy, black nylon leash. Can be used with dogs or cats up to 80 pounds.',
 	'12.99',
 	'400',
 	'https://images2.imgbox.com/0f/e3/T4jkbFKB_o.png' 
@@ -88,7 +88,7 @@ INSERT INTO Products
 VALUES
 (	
 	'Food Mat',
-	' ',
+	'Give your buddy the best eating experience possible with this food mat. Not only is it an amazing anti-slip surface for underneath their bowl, it catches any spills and keeps your floor clean!',
 	'28.99',
 	'429',
 	'https://images2.imgbox.com/52/f2/3TxdsyIy_o.png' 
@@ -100,7 +100,7 @@ INSERT INTO Products
 VALUES
 (
 	'Pet Bed (Small)',
-	' ',
+	'This brown, circular pet bed is designed with the comfort of your pet in mind. The fluffy interior provides warmth while the shape allows your dog or cat to curl up comfortably inside. Measures 2 feet in diameter.',
 	'99.99',
 	'300',
 	'https://images2.imgbox.com/6d/e4/iI6K6W96_o.png'
@@ -112,7 +112,7 @@ INSERT INTO Products
 VALUES
 (	
 	'Pet Bed (Large)',
-	' ',
+	'This fun cow-print rectangular ped is perfect for your large pet. Memory foam inside eases their joints, and the large surface area provides plenty of room to relax. The cover is removable and machine washable. Measures 4 feet by 3 feet.',
 	'150.99',
 	'350',
 	'https://images2.imgbox.com/9b/e8/m8Ib3y5E_o.png' 
@@ -124,7 +124,7 @@ INSERT INTO Products
 VALUES
 (
 	'Brush',
-	' ',
+	'A double-sided brush with high quality wooden handle. Perfect for all coats. Designed to fit comfortably and ergonomically in your hand as you groom your furry friend.',
 	'9.99',
 	'400',
 	'https://images2.imgbox.com/98/af/LzKtEsvQ_o.png'
@@ -136,7 +136,7 @@ INSERT INTO Products
 VALUES
 (
 	'Pet Cologne',
-	' ',
+	'This cologne will keep your male furry friend smelling great for weeks! The scent is inspired by a rainforest, with hints of eucalyptus and light citrus. Made with aloe to nourish your pet''s coat.',
 	'20.99',
 	'500',
 	'https://images2.imgbox.com/c9/2d/KgMapJ2g_o.png' 
@@ -148,7 +148,7 @@ INSERT INTO Products
 VALUES
 (
 	'Pet Perfume',
-	' ',
+	'This perfume will freshen up your female furry friend for weeks! The scent is vanilla almond. Formula includes oatmeal to nourish your pet''s coat.',
 	'25.99',
 	'400',
 	'https://images2.imgbox.com/1d/89/oRhBNPYl_o.png'
@@ -160,7 +160,7 @@ INSERT INTO Products
 VALUES
 (
 	'Logo Pet Hoodie (Small)',
-	' ',
+	'A gray pet hoodie with the Bob''s logo printed on. Recommended for small dog breeds and cats. Machine washable.',
 	'30.50',
 	'100',
 	'https://images2.imgbox.com/9a/d4/FpheE4Yi_o.png'
@@ -172,7 +172,7 @@ INSERT INTO Products
 VALUES
 (	
 	'Logo Pet Hoodie (Large)',
-	' ',
+	'A gray pet hoodie with the Bob''s logo printed on. Recommended for large dog breeds. Machine washable.',
 	'35.50',
 	'150',
 	'https://images2.imgbox.com/bb/73/qgLXC5PE_o.png' 
@@ -184,7 +184,7 @@ INSERT INTO Products
 VALUES
 (	
 	'Hot Dog Toy (Squeaky)',
-	' ',
+	'Meet your best friend''s new favorite toy! This squeaky hot dog is perfect to keep them occupied, especially during the summer time. Designed to endure chewing and slobbering.',
 	'69.99',
 	'600',
 	'https://images2.imgbox.com/40/05/HArcYloD_o.png'
@@ -196,7 +196,7 @@ INSERT INTO Products
 VALUES
 (	
 	'Tarantula',
-	' ',
+	'A large, adult male tarantula ready to be your next best friend! May vary in size, between 5 and 7 inches including legspan. Guaranteed live delivery.',
 	'150.00',
 	'10',
 	'https://images2.imgbox.com/4a/27/7SdP91xU_o.png' 
@@ -208,7 +208,7 @@ INSERT INTO Products
 VALUES
 (
 	'Chinchilla',
-	' ',
+	'An adult female chinchilla, ready to provide lots of love! May vary in size, between 9 and 14 inches. Guaranteed live delivery.',
 	'200.00',
 	'12',
 	'https://images2.imgbox.com/75/94/xdIsSm6a_o.png'
@@ -220,7 +220,7 @@ INSERT INTO Products
 VALUES
 (
 	'Baby Fox',
-	' ',
+	'An energetic baby fox! Gender may vary, along with size and coloring. Guaranteed live delivery. May grow to be 8 to 15 inches tall.',
 	'499.99',
 	'13',
 	'https://images2.imgbox.com/84/1b/TpaehIoJ_o.png' 
@@ -232,7 +232,7 @@ INSERT INTO Products
 VALUES
 (
 	'Chameleon',
-	' ',
+	'A colorful male chameleon! This camoflauging friend will be the perfect addition to your household! May vary in size, between 12 and 20 inches. Guaranteed live delivery.',
 	'299.99',
 	'5',
 	'https://images2.imgbox.com/87/94/2hgIpDhh_o.png'
@@ -244,7 +244,7 @@ INSERT INTO Products
 VALUES
 (	
 	'Bearded Dragon (Bob-Made)',
-	' ',
+	'This bearded dragon is specially Bob-made, with Bob''s proprietary formulas. Size may vary between 12 and 19 inches, along with gender. Guaranteed live delivery.',
 	'999.99',
 	'6',
 	'https://images2.imgbox.com/eb/03/cR7fFMle_o.png' 
